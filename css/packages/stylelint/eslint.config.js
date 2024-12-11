f442ea9ece49edabfa8fe2e6b78a0bf97e89e651
Max Degterev <max@degterev.me>
2024-12-11 19:32:37 +0100

    fixes

---
+++ css/packages/stylelint/eslint.config.js
@@ -0,0 +1,3 @@
+import baseConfig from 'entva-base';
+
+export default baseConfig;
--- test/css.js
+++ test/css.js
@@ -1,6 +1,15 @@
-const { join } = require('path');
-const stylelint = require(require.resolve('stylelint', { paths: [process.cwd()] }));
-const { collectLinterErrors, createTestErrorsCollector } = require('./utils');
+import { resolve } from 'path';
+import fs from 'fs';
+import { collectLinterErrors, createTestErrorsCollector } from './utils.js';
+
+// ESM is very convenient and doesn't require any workarounds for the CJS dynamic import
+const loadStyleLint = async () => {
+  const cwd = process.cwd();
+  const pkg = JSON.parse(fs.readFileSync(resolve(cwd, 'node_modules/stylelint/package.json'), 'utf8'));
+  const styleLint = resolve(cwd, 'node_modules/stylelint', pkg.main);
+  const mod = await import(styleLint);
+  return mod.default;
+};
 
 
 const processFile = (result) => {
@@ -10,10 +19,11 @@ const processFile = (result) => {
   if (errorsTest.length) throw `Errors found:\n${errorsTest.join('\n')}`;
 };
 
-module.exports = async (dir, exts = 'css,scss') => {
+const run = async (dir, exts = 'css,scss') => {
+  const stylelint = await loadStyleLint();
   try {
     const report = await stylelint.lint({
-      files: exts.split(',').map((ext) => join(dir, `**/**.${ext}`)),
+      files: exts.split(',').map((ext) => `${dir}/**/*.${ext}`),
       quietDeprecationWarnings: true,
     });
     report.results.forEach(processFile);
@@ -22,3 +32,5 @@ module.exports = async (dir, exts = 'css,scss') => {
     process.exit(1);
   }
 };
+
+export default run;
