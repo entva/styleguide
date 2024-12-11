a052a7d3bc5f86e2503b01fcc07e648eb3727726
Max Degterev <max@degterev.me>
2024-12-11 19:47:50 +0100

    fixes

---
+++ tooling/packages/biome/test/invalid/misc.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-unused-vars */
+
+export type MyType = {
+  // expect: @stylistic/ts/member-delimiter-style
+  prop: string;
+};
+
+// expect: @typescript-eslint/no-explicit-any
+const age: any = 'seventeen';
+
+// expect: @typescript-eslint/no-explicit-any
+type Anything = any;
