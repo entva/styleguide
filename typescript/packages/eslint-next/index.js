6c63660a292d718b27f5f8d81bd2c5cab5489631
Max Degterev <max@degterev.me>
2024-12-16 19:42:26 +0100

    fixes

--- typescript/packages/eslint-next/index.js
+++ typescript/packages/eslint-next/index.js
@@ -4,12 +4,14 @@ import { FlatCompat } from '@eslint/eslintrc';
 const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 export default [
-  ...compat.config({ extends: ['next/core-web-vitals'] }),
+  ...compat.config({ extends: ['next/core-web-vitals', 'next/typescript'] }),
   ...baseConf,
   {
     files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
     rules: {
-      '@next/next/no-img-element': ['off'],
+      '@next/next/no-img-element': [0],
+      '@typescript-eslint/no-non-null-asserted-optional-chain': [0],
+      '@typescript-eslint/no-unnecessary-type-constraint': [0],
     },
   },
 ];
